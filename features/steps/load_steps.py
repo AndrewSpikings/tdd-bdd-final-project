d151d8738ef52836669b0d88e70f578aa1505436
AndrewSpikings <andrew.spikings@procode.co.uk>
2024-11-28 12:12:11 +0000

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -48,13 +48,13 @@ def step_impl(context):
     #
     for row in context.table:
         payload = {
-            "name": row["name"],
-            "description": row["name"],
-            "price": row["price"],
-            "available": row["available"],
-            "category":  row["category"]
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
         }
-        context.resp = requests.post(rest_endpoint,payload)
-        assert(context.resp.status_code == HTTP_201_CREATED)
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
           
     
