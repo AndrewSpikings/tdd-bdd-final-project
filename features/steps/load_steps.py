0824b272a25dac6636eca463d288e8d52ca11a96
AndrewSpikings <andrew.spikings@procode.co.uk>
2024-11-28 11:38:33 +0000

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -47,6 +47,14 @@ def step_impl(context):
     # load the database with new products
     #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row["name"],
+            "description": row["name"],
+            "price": row["price"],
+            "available": row["available"],
+            "category":  row["category"]
+        }
+        context.resp = requests.post(rest_endpoint,payload)
+        assert(context.resp.status_code == HTTP_201_CREATED)
+          
+    
